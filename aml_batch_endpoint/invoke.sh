2461492491decd9d52209ab50ba22f02364bf188
Bea Stollnitz <bea@stollnitz.com>
2022-07-11 18:27:22 -0700

    Got invoke.sh working.

--- aml_batch_endpoint/invoke.sh
+++ aml_batch_endpoint/invoke.sh
@@ -1,6 +1,6 @@
 ENDPOINT_NAME=endpoint-batch
 DATASET_NAME=data-invoke-batch
-DATASET_VERSION=1
+DATASET_VERSION=2
 
 SUBSCRIPTION_ID=$(az account show --query id | tr -d '\r"')
 echo "SUBSCRIPTION_ID: $SUBSCRIPTION_ID"
@@ -17,6 +17,9 @@ echo "SCORING_URI: $SCORING_URI"
 SCORING_TOKEN=$(az account get-access-token --resource https://ml.azure.com --query accessToken -o tsv)
 echo "SCORING_TOKEN: $SCORING_TOKEN"
 
+INPUT_PATH=$(az ml data show -n $DATASET_NAME -v $DATASET_VERSION --query path -o tsv)
+echo "INPUT_PATH: $INPUT_PATH"
+
 curl --location --request POST $SCORING_URI \
 --header "Authorization: Bearer $SCORING_TOKEN" \
 --header "Content-Type: application/json" \
@@ -24,13 +27,13 @@ curl --location --request POST $SCORING_URI \
     \"properties\": {
         \"inputData\": {
             \"uriFolder\": {
-                \"uri\": \"azureml://subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/workspaces/$WORKSPACE/data/$DATASET_NAME/versions/$DATASET_VERSION/\",
+                \"uri\": \"$INPUT_PATH\",
                 \"jobInputType\": \"UriFolder\",
             }
         },
         \"outputData\": {
             \"uriFile\": {
-                \"uri\": \"azureml://subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/workspaces/$WORKSPACE/datastores/workspaceblobstore/batch-endpoint-output\",
+                \"uri\": \"azureml://datastores/workspaceblobstore/paths/$ENDPOINT_NAME/batch-endpoint-output.csv\",
                 \"jobOutputType\": \"UriFile\",
             }
         },
